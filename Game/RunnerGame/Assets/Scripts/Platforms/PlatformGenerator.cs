dd59bcc55cc9ed485d8fa546d31cbe0b401a8cbe
kevkuntz <kkuntz@htwsaar.de>
2017-11-29 14:25:53 +0100

    Camera movement, player controller, generating different platform sizes

---
+++ Game/RunnerGame/Assets/Scripts/Platforms/PlatformGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform; 
+	public Transform generationPoint; 
+	public float distanceBetween;
+
+	private float platformWidth; 
+
+	public float distanceBetweenMin; 
+	public float distanceBetweenMax;
+
+
+
+	public GameObject[] thePlatforms; 
+	private int platformSelector; 
+	private float[] platformWidths; 
+	private int gapChoice; 
+
+	public GameObject lastPlatform; 
+	//public ObjectPooler theObjectPool; 
+
+	// Use this for initialization
+	void Start () {
+		//platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
+		platformWidths = new float[thePlatforms.Length];
+
+		for(int i = 0; i < thePlatforms.Length; i++){
+			platformWidths[i] = thePlatforms [i].GetComponent  <BoxCollider2D>().size.x; 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.x < generationPoint.position.x) {
+
+			distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
+
+			gapChoice = Random.Range (0, 5);
+
+			switch((int) lastPlatform.GetComponent<BoxCollider2D>().size.y){
+			case 2:
+				platformSelector = Random.Range (1,4);
+				break; 
+			case 3: 
+				platformSelector = Random.Range (1,4);
+				break; 
+			case 4: 
+				platformSelector = Random.Range (2,5);
+				break;
+			case 5: 
+				platformSelector = Random.Range (3,5);
+				break;
+			default:
+				platformSelector = Random.Range (0, 4);
+				break;
+			}
+
+
+			if (gapChoice < 4) {	
+				transform.position = new Vector3 (transform.position.x + platformWidths [platformSelector], transform.position.y, 0); 
+			} else {
+				transform.position = new Vector3 (transform.position.x + platformWidths [platformSelector] + distanceBetween, transform.position.y, 0);
+			}
+
+
+			Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
+
+			lastPlatform = thePlatforms[platformSelector];
+//			GameObject newPlatform = theObjectPool.GetPooledObject ();
+//			newPlatform.transform.position = transform.position; 
+//			newPlatform.transform.rotation = transform.rotation; 
+//			newPlatform.SetActive (true);
+
+		}
+		
+	}
+}
---
+++ Game/RunnerGame/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed ; 
+	public float jumpForce ; 
+
+	private Rigidbody2D myRigidbody; 
+
+	public bool grounded;
+	public LayerMask whatIsGround;
+	public CameraController mainCamera; 
+
+	private Collider2D myCollider; 
+
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+		myCollider = GetComponent<Collider2D> (); 
+		mainCamera = FindObjectOfType<CameraController> ();
+		moveSpeed = mainCamera.cameraSpeed *1.5f ;
+		 
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		grounded = Physics2D.IsTouchingLayers (myCollider, whatIsGround);
+
+		//Debug.Log (mainCamera.cameraSpeed);
+		moveSpeed = mainCamera.cameraSpeed *1.2f ;
+
+
+		//myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);
+		//myRigidbody.velocity  = mainCamera.myCamera.velocity;
+
+//		if(Input.GetKeyDown (KeyCode.RightArrow)){
+//			//myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y) ;
+//
+//
+//		}
+
+		this.transform.Translate (Input.GetAxisRaw ("Horizontal") * Time.deltaTime * moveSpeed, 0, 0);
+	
+//		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+//			myRigidbody.velocity = new Vector2 (-moveSpeed, myRigidbody.velocity.y);
+//		}
+//
+
+		if(Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0) ){
+			if (grounded) {
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+			}
+		}
+	}
+
+}
